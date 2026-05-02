b8b08bcf9cb4f5688e61e46c1dda63da337a3fa0
Pyrathi <neo.carlier2212@gmail.com>
2026-05-02 14:57:15 +0000

    nettoyage fichier+ début modèle users

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,27 +1,21 @@
 import { Sequelize } from "sequelize";
+import 'dotenv/config'; 
 
-const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL, {
-      dialect: "postgres",
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      },
-      logging: false,
-    })
-  : new Sequelize("postgres","postgres.vhahqttkyidjsdnwtmav", "Nescha50jo.s", {
-      host: "aws-1-eu-central-1.pooler.supabase.com",
-      port:  5432,
-      dialect: "postgres",
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      },
-      logging: false,
-    });
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("ERREUR : DATABASE_URL est manquante dans le fichier .env");
+}
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, 
+    },
+  },
+  logging: false, 
+});
 
 export default sequelize;
